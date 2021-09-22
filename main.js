4815d9eba3c037670cb2fefb96556d36b54b5f04
sai135 <83128908+sai135@users.noreply.github.com>
2021-09-21 20:21:28 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function nextpage() {
+window.location="nextpage.html";
+}
+function anotherpage() {
+window.location="anotherpage.html";
+}
+function click1() {
+window.location="anotherpage.html";
+}
+function click2() {
+    window.location="about_soil.html";
+    }
+    function click3() {
+    window.location="plants.html"
+    }
